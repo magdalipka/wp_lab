6d568acbf8d38b9b6d9ae4b439b967a30b4a23eb
Magda Lipka <dev@magdalipka.com>
2021-11-26 12:28:17 +0100

    7

---
+++ 7_bridge.ts
@@ -0,0 +1,55 @@
+interface DateFormat {
+  write(y: string, m: string, d: string): void;
+}
+
+class YMD implements DateFormat {
+  #fs: FileWriter;
+  constructor(fs: FileWriter) {
+    this.#fs = fs;
+  }
+  write(y: string, m: string, d: string) {
+    this.#fs.write(`${y}${m}{d}`);
+  }
+}
+
+class MDY implements DateFormat {
+  #fs: FileWriter;
+  constructor(fs: FileWriter) {
+    this.#fs = fs;
+  }
+  write(y: string, m: string, d: string) {
+    this.#fs.write(`${m}${d}${y}`);
+  }
+}
+
+class DMY implements DateFormat {
+  #fs: FileWriter;
+  constructor(fs: FileWriter) {
+    this.#fs = fs;
+  }
+  write(y: string, m: string, d: string) {
+    this.#fs.write(`${d}${m}${y[3]}${y[4]}`);
+  }
+}
+
+interface FileWriter {
+  write(input: string): void;
+}
+
+class CSVWriter implements FileWriter {
+  write(input: string) {
+    console.log("writing csv...");
+    console.log(input);
+  }
+}
+
+class XMLWriter implements FileWriter {
+  write(input: string) {
+    console.log("writing xml...");
+    console.log(input);
+  }
+}
+
+const ymd = new YMD(new CSVWriter());
+
+ymd.write("");
